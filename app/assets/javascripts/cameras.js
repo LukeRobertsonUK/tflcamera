a257be2fd49ca126f7dfb0527f908141c3df50d6
Luke Robertson <luke.robertson@mac.com>
2013-07-31 09:49:16 +0100

    refactor to move info window outside the loop

--- app/assets/javascripts/cameras.js
+++ app/assets/javascripts/cameras.js
@@ -10,20 +10,22 @@ function initialize() {
 
   var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
+  var infowindow = new google.maps.InfoWindow({
+          content: ""
+        });
+
 //now get the cameras array from the server using JSON
 //see how we serve up the JSON object in the rails cameras controller
-  var cameras;
-  $.getJSON('/cameras.json', function(data){
-    // everything you do with the JSON object happens inside this function
-    cameras = data
 
+  $.getJSON('/cameras.json', function(cameras){
+    // everything you do with the JSON object happens inside this function
     //loop through my cameras array creating a marker for each one.
     for(var i =0; i < cameras.length; i++){
 
       var marker = new google.maps.Marker({
         position: new google.maps.LatLng(parseFloat(cameras[i].lat), parseFloat(cameras[i].lng)),
         map: map,
-        title: 'placeholder',
+        title: cameras[i].location,
         location: cameras[i].location,
         postcode: cameras[i].postcode,
         file: cameras[i].file
@@ -32,18 +34,16 @@ function initialize() {
       //create an event to happen on clicking each marker
       google.maps.event.addListener(marker, 'click', function() {
 
-        //write a content string for the marker we are currently iterating...
-        var contentString = '<div id="content"><img src="http://www.tfl.gov.uk/tfl/livetravelnews/trafficcams/cctv/' + this.file + '"><p>' + this.location + ", " + this.postcode + '</p></div>';
 
-        //... and use this string to create a new content window for this marker
-        var infowindow = new google.maps.InfoWindow({
-          content: contentString
-        });
+        //write a content string for the marker we are currently iterating and add it to the info window we created earlier
+        infowindow.content = '<div id="content"><img src="http://www.tfl.gov.uk/tfl/livetravelnews/trafficcams/cctv/' + this.file + '"><p>' + this.location + ", " + this.postcode + '</p></div>';
+
+
 
         //finally, define what happens when we click the marker
         infowindow.open(map, this)
 
-      });  //closes the goolge maps listener event
+      });  //closes the google maps listener event
     } // closes the for loop
   }) //closes the getJSON
 } //closes initialize function
